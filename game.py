605becabfad7c7f1ccc789b7924dec9a524b38fb
Shirley Wang <shirley.wang.xinli@gmail.com>
2023-11-26 21:18:00 -0800

    [Game Implementation] Implement printing virtual map and special location inside

--- game.py
+++ game.py
@@ -11,15 +11,62 @@ ROOM_TYPE_CLASS_ROOM = 2
 ROOM_TYPE_RELAX = 3
 ROOM_TYPE_BOSS_ROOM = 4
 
-SCIENCE = "Science"
-GEOGRAPHY = "Geography"
-COMPUTER_SCIENCE = "Computer Science"
+SCIENCE = ("Science", "Sci")
+GEOGRAPHY = ("Geography", "Geo")
+COMPUTER_SCIENCE = ("Computer Science", "C&S")
 SUBJECTS = (SCIENCE, GEOGRAPHY, COMPUTER_SCIENCE)
 
 MAXIMUM_LEVEL = 3
 EXPERIENCE_FOR_LEVEL_UP = 10
 
 USER_INFORMATION_FILE = "user_info.json"
+
+
+def get_room_title_and_grade(game_map, row, column):
+    if game_map[(row, column)]["type"] == ROOM_TYPE_CLASS_ROOM:
+        room_title = " {}{} ".format(game_map[(row, column)]["subject"][1], game_map[(row, column)]["subject_grade"])
+        return room_title
+    elif game_map[(row, column)]["type"] == ROOM_TYPE_ENTRANCE:
+        return  "\t🚪\t"
+    elif game_map[(row, column)]["type"] == ROOM_TYPE_RELAX:
+        return "\t🍀\t"
+    elif game_map[(row, column)]["type"] == ROOM_TYPE_BOSS_ROOM:
+        return " !😈!\t"
+    else:
+        return ""
+
+
+
+def print_map(game_map,character):
+    for row in range(MAP_ROW_NUMBER):
+        line_1 =""
+        line_2 =""
+        line_3 =""
+        line_4 =""
+        for column in range(MAP_COLUMN_NUMBER):
+            if character["location"] == (row, column):
+                line_1 += "╔════════╗"
+                line_2 += "║ {} ║".format(get_room_title_and_grade(game_map, row, column))
+                line_3 += "║ {}║".format("⎝(◕_◕)⎠")
+                line_4 += "╚════════╝"
+            else:
+                line_1 += "┌╌╌╌╌╌╌╌╌┐"
+                line_2 += "┆ {} ┆".format(get_room_title_and_grade(game_map, row, column))
+                line_3 += "┆        ┆"
+                line_4 += "└╌╌╌╌╌╌╌╌┘"
+        print(line_1)
+        print(line_2)
+        print(line_3)
+        print(line_4)
+
+
+
+def print_map_and_current_location(game_map, character):
+    # Draw map and contain character inside
+    print_map(game_map,character)
+    # Print details of current location
+    print("Welcome to {}, {}".format(game_map["type"],character["user_name"]))
+
 
 
 def subjects():
@@ -40,8 +87,8 @@ def generate_map():
     game_map ={
         (row,column):{
             "type": ROOM_TYPE_CLASS_ROOM,
-            "subject": subjects(), # more subjects
-            "subject_grade": subject_grades(row,column), # grades:2 to 4
+            "subject": subjects(),
+            "subject_grade": subject_grades(row,column),
             "completed": False
         }
         for row in range(0,MAP_ROW_NUMBER) for column in range(0,MAP_COLUMN_NUMBER)
